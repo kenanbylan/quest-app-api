521d94603f81e69e9472106f181873c67317d15f
Kenan Baylan <kenan.baylan4654@gmail.com>
2023-07-07 22:38:44 +0300

    feat: request and resposne layer added.

--- src/main/java/com/springproject/QuestApp/controllers/PostController.java
+++ src/main/java/com/springproject/QuestApp/controllers/PostController.java
@@ -2,6 +2,7 @@ package com.springproject.QuestApp.controllers;
 
 import com.springproject.QuestApp.entities.Post;
 import com.springproject.QuestApp.requests.PostCreateRequest;
+import com.springproject.QuestApp.requests.PostUpdateRequest;
 import com.springproject.QuestApp.services.PostService;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,7 +36,15 @@ public class PostController {
     }
 
 
+    @PutMapping("/{postId}")
+    public Post updateOnePost(@PathVariable Long postId) {
+        return  postService.updateOnePostById(postId);
+    }
 
 
+    @DeleteMapping("/{postId}")
+    public void deleteOnePost(@PathVariable Long postId, @RequestBody PostUpdateRequest postUpdateRequest) {
+        return; postService.deleteOnePostById(postId,postUpdateRequest);
+    }
 
 }
